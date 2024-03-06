729ef21525442df8febe934b8af144e54dfaa3ba
Matthew R. Becker <beckermr@users.noreply.github.com>
2024-03-06 09:09:16 -0600

    BUG fix tests

--- test_load_graph.py
+++ test_load_graph.py
@@ -1,11 +1,11 @@
 import logging
-from conda_forge_tick.utils import setup_logger
+from conda_forge_tick.utils import setup_logging
 from conda_forge_tick.make_graph import make_graph
 from conda_forge_tick.utils import load_graph
 
 
 def test_load_graph():
-    setup_logger(logging.getLogger("conda_forge_tick"), level="DEBUG")
+    setup_logging(level="DEBUG")
     names = []
     gx = load_graph()
     gx = make_graph(names, gx)
